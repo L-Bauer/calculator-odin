d3cada7dcc21a4f989b108f0233c78ed9d18a5dd
Lucas D. Bauer <lucassoccer95@gmail.com>
2022-08-02 17:12:34 -0500

    Implement math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+//Math Functions - addition, subtraction, multiplication, division 
+function add(a,b) {
+    c = a + b;
+    return c
+}
+
+function sub(a,b) {
+    c = a - b;
+    return c
+}
+
+function multi(a,b) {
+    c = a * b;
+    return c
+}
+
+function div(a,b) {
+    c = a / b;
+    return c
+}
